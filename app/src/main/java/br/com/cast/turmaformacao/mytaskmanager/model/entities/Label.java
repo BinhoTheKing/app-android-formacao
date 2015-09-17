ce2995b6420a747833d9869ecc7f87ec7410b38c
Fabio <fabioamorim1501@gmail.com>
2015-09-17 11:32:04 -0300

    Aula 4

--- app/src/main/java/br/com/cast/turmaformacao/mytaskmanager/model/entities/Label.java
+++ app/src/main/java/br/com/cast/turmaformacao/mytaskmanager/model/entities/Label.java
@@ -3,4 +3,8 @@ package br.com.cast.turmaformacao.mytaskmanager.model.entities;
 public class Label {
     private int id;
     private String name;
+
+    public Label(){
+        super();
+    }
 }
