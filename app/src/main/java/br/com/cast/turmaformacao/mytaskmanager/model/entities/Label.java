036e8a7bb77ea039d596aab1881607f2b2d7aea7
Fabio <fabioamorim1501@gmail.com>
2015-09-21 10:46:27 -0300

    ColorPicker working!! :)

--- app/src/main/java/br/com/cast/turmaformacao/mytaskmanager/model/entities/Label.java
+++ app/src/main/java/br/com/cast/turmaformacao/mytaskmanager/model/entities/Label.java
@@ -1,6 +1,9 @@
 package br.com.cast.turmaformacao.mytaskmanager.model.entities;
 
-public class Label {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Label implements Parcelable {
     private Integer id;
     private String name;
     private String description;
@@ -68,4 +71,36 @@ public class Label {
         result = 31 * result + name.hashCode();
         return result;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.id == null ? -1 : this.id);
+        dest.writeString(this.name == null ? "" : this.name);
+        dest.writeString(this.description == null ? "" : this.description);
+        dest.writeInt(this.color == null ? -1 : this.color.ordinal());
+    }
+
+    protected Label(Parcel in) {
+        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.id = this.id == -1 ? null : this.id;
+        this.name = in.readString();
+        this.description = in.readString();
+        int tmpColor = in.readInt();
+        this.color = tmpColor == -1 ? null : Color.values()[tmpColor];
+    }
+
+    public static final Parcelable.Creator<Label> CREATOR = new Parcelable.Creator<Label>() {
+        public Label createFromParcel(Parcel source) {
+            return new Label(source);
+        }
+
+        public Label[] newArray(int size) {
+            return new Label[size];
+        }
+    };
 }
